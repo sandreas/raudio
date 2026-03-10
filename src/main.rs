d7de30f1eefaa28a395a110bfa4c2b127fb1ff8d
sandreas <2050604+sandreas@users.noreply.github.com>
2026-03-10 13:04:22 +0100

    added some background stuff and player task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
+use crate::player::{Player, PlayerCommand, PlayerEvent};
+use clap::Parser;
+use std::{io, thread};
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+mod player;
+
+#[derive(Parser, Debug)]
+#[command(version, about = "rodio example", long_about = None)]
+struct Opt {
+    /// The audio device to use
+    #[arg(short, long)]
+    device: Option<String>,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let (_player_cmd_tx, player_cmd_rx) = tokio::sync::mpsc::unbounded_channel::<PlayerCommand>();
+    // let player_cmd_tx_shared = Arc::new(player_cmd_tx.clone());
+
+    let (player_evt_tx, mut _player_evt_rx) = mpsc::unbounded_channel::<PlayerEvent>();
+
+    start_tokio_background_tasks(player_cmd_rx, player_evt_tx);
+
+    let mut quit = false;
+    while !quit {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+
+        match buffer.trim() {
+            "a" => println!("You pressed A"),
+            "b" => println!("You pressed B"),
+            "q" => quit = true,
+            _ => println!("Other key"),
+        }
+    }
+
+
+
+}
+
+
+pub fn start_tokio_background_tasks(
+    player_rx: UnboundedReceiver<PlayerCommand>,
+    player_evt_tx: UnboundedSender<PlayerEvent>,
+) {
+    println!("=== start_tokio_background_tasks");
+    thread::spawn(move || {
+        tokio::runtime::Runtime::new().unwrap().block_on(background_tasks(
+            player_rx,
+            player_evt_tx,
+        ));
+    });
+
+
+}
+
+pub async fn background_tasks(
+    player_rx: UnboundedReceiver<PlayerCommand>,
+    player_evt_tx: UnboundedSender<PlayerEvent>,
+) {
+
+    let player_task = tokio::spawn(async {
+        let sink: Option<&rodio::Player> = None;
+        let _ = Player::new().run(sink, player_rx, player_evt_tx).await;
+    });
+
+
+
+
+
+
+    let _ = tokio::join!(player_task);
+
+
+
 }
---
+++ src/player.rs
@@ -0,0 +1,97 @@
+use rodio::Source;
+use std::fs::File;
+use std::path::Path;
+use std::time::Duration;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+
+#[derive(Debug)]
+pub enum PlayerCommand {
+    PlayTest(),
+    PlayMedia(String, Duration),
+}
+
+#[derive(Debug)]
+pub enum PlayerEvent {
+    Position(String, Duration),
+}
+
+pub struct Player {}
+
+impl Player {
+    pub fn new() -> Player {
+        Self {}
+    }
+
+    pub async fn run(
+        &mut self,
+        sink_option: Option<&rodio::Player>,
+        mut cmd_rx: UnboundedReceiver<PlayerCommand>,
+        evt_tx: UnboundedSender<PlayerEvent>,
+    ) {
+        loop {
+            let mut last_player_pos = Duration::from_secs(0);
+            if let Some(sink) = sink_option {
+                tokio::select! {
+
+
+                    Some(cmd) = cmd_rx.recv() => {
+                        println!("============== cmd received ==============");
+                        match cmd {
+                            PlayerCommand::PlayTest() => {
+                                sink.clear();
+                                let waves = vec![230f32, 270f32, 330f32, 270f32, 230f32];
+                                for w in waves {
+                                    let source = rodio::source::SineWave::new(w).amplify(0.1);
+                                    sink.append(source);
+                                    sink.play();
+                                    tokio::time::sleep(Duration::from_millis(200)).await;
+                                    sink.stop();
+                                    sink.clear();
+                                }
+                            }
+                            PlayerCommand::PlayMedia(s, position) => {
+                                let path = Path::new(s.as_str());
+                                let file_result = File::open(path);
+
+                                if let Ok(file) = file_result {
+                                    let decoder_result = rodio::Decoder::try_from(file);
+                                    if let Ok(decoder) = decoder_result{
+                                        sink.clear();
+                                        sink.append(decoder);
+                                        if position > Duration::from_secs(0) {
+                                            let _ = sink.try_seek(position);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    _ = tokio::time::sleep(Duration::from_millis(500)) => {
+                        let pos = sink.get_pos();
+
+                        if pos != last_player_pos {
+                            self.update_position(&evt_tx, pos).await;
+                            last_player_pos = pos;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    async fn update_position(&self, _evt_tx: &UnboundedSender<PlayerEvent>, pos: Duration) {
+        println!("position: {}", format_duration(pos));
+    }
+}
+
+
+pub fn format_duration(duration: Duration) -> String {
+    let millis = duration.as_millis();
+    let secs = millis / 1000;
+    let h = secs / (60 * 60);
+    let m = (secs / 60) % 60;
+    let s = secs % 60;
+    format!("{:0>2}:{:0>2}:{:0>2}", h, m, s)
+}
